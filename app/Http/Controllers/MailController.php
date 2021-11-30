7fe27459472f697af34f15b269a2db0c353b49ae
MOT EL <mot_edu@outlook.com>
2021-11-30 19:22:05 +0700

    Update

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -18,12 +18,6 @@ class MailController extends Controller
 
     public function send(){
         $contact = [
-            ['email'=>'anthony.thong@softlinegroup.com','name'=>'Mr Anthony Thong'],
-            ['email'=>'daron.wong@zicolaw.com','name'=>'Mr Daron Wong'],
-            ['email'=>'business@speedpayplc.com','name'=>'Mr Lee Kok Yang'],
-            ['email'=>'ssm.e@online.com.kh','name'=>'Ir. E.K.CHEW'],
-            ['email'=>'thiangyh@sunwayhotels.com','name'=>'Mr Thiang Yang Hian'],
-            ['email'=>'benjamin@t2interavtive.com','name'=>'Mr. Benjamin Chong'],
             ['email'=>'houtkimmeng@tanchonggroup.com','name'=>'Mr Huot Kimmeng'],
             ['email'=>'wisemac1998@hotmail.com','name'=>'Mr. Ng Hui'],
             ['email'=>'khorwohock@gmail.com','name'=>'Khor Woh Hock'],
